13b00e80a2fb55acff3891624f9456a13a1d631b
nerivsm <46284299+nerivsm@users.noreply.github.com>
2019-01-02 15:40:43 -0600

    Create try.cpp

---
+++ try.cpp
@@ -0,0 +1,73 @@
+/*
+#include <iostream>     // std::cout
+#include <algorithm>    // std::for_each
+#include <vector>       // std::vector
+
+void myfunction (int i) {  // function:
+  std::cout << ' ' << i;
+}
+
+struct myclass {           // function object type:
+  void operator() (int i) {std::cout << ' ' << i;}
+} myobject;
+
+int main () {
+  std::vector<int> myvector;
+  myvector.push_back(10);
+  myvector.push_back(20);
+  myvector.push_back(30);
+
+  std::cout << "myvector contains:";
+  for_each (myvector.begin(), myvector.end(), myfunction);
+  std::cout << '\n';
+
+  // or:
+  std::cout << "myvector contains:";
+  for_each (myvector.begin(), myvector.end(), myobject);
+  std::cout << '\n';
+
+    int arr[] = {1,2,3,4,5};
+    std::cout << "array contains:";
+    std::for_each (std::begin(arr), std::end(arr), myfunction);
+    std::cout << '\n';
+
+  return 0;
+}
+*/
+
+/*
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    auto func = [] () { cout << "Hello world"; };
+    func(); // now call the function
+}
+*/
+
+#include <iostream>
+using namespace std; 
+int main()
+{
+    int from=0, to=8; 
+    int count = to - from + 1; 
+    int n = (count + 7) / 8;
+    cout<<"count: "<<count<<", n: "<<n<<", remainder: "<<count%8<<"\n";
+
+    switch (count % 8) 
+    {
+        case 0: do { to = from++; cout<<"case 0\n"; 
+        case 7:      to = from++; cout<<"case 7\n";
+        case 6:      to = from++; cout<<"case 6\n";
+        case 5:      to = from++; cout<<"case 5\n";
+        case 4:      to = from++; cout<<"case 4\n";
+        case 3:      to = from++; cout<<"case 3\n";
+        case 2:      to = from++; cout<<"case 2\n";
+        case 1:      to = from++; cout<<"case 1\n";
+                } while (--n > 0);
+    }
+
+    return 0;
+}
